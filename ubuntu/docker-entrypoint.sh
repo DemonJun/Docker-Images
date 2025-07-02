000bcbbe939a2439a5e38cc36aae338dd4b568c8
范俊翔 <xm1107536827@gmail.com>
2025-07-02 12:23:13 +0800

    update docker-entrypoint.sh to set tool versions for Java, Maven, and Node.js

--- ubuntu/docker-entrypoint.sh
+++ ubuntu/docker-entrypoint.sh
@@ -3,9 +3,15 @@
 # 加载环境变量和配置
 source /root/.config/fish/config.fish
 
+cat <<EOF > /root/.version-fox/.tool-versions
+java 17.0.14+7-tem
+maven 3.9.9
+nodejs 20.19.3
+EOF
+
 vfox use java@17.0.14+7-tem -g
-vfox use maven@3.9.9 -g 
-vfox use nodejs@23.9.0 -g
+vfox use maven@3.9.9 -g
+vfox use nodejs@20.19.3 -g
 
 # 如果 ENABLE_SSH 为 true，则启动 SSH 服务
 if test "$ENABLE_SSH" = "true"
